74b67e4c9aa045ad23ac83ef9e1fadf668cb608e
anry88 <anry15@mail.ru>
2018-06-03 23:59:21 +0300

    task1 complete

--- task1.rb
+++ task1.rb
@@ -1,1 +1,12 @@
-print 1
+n=ARGV[0].to_i
+e=ARGV[1].to_i
+count = 1
+sum = 0
+
+if 1 <= n and e <= 100
+    while count <= n
+        sum = sum + (count**e)
+        count = count + 1
+    end
+end
+puts sum
