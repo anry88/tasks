feba4aca6bb59aa56808bf40cf217214dcf59347
anry88 <anry15@mail.ru>
2018-06-04 16:36:58 +0300

    task2 complete

---
+++ task2.rb
@@ -0,0 +1,29 @@
+s=ARGV[0]
+k=ARGV[1].to_i
+n=s.size
+array = []
+
+ def encrypt(s, k, array)
+    s.each_byte do |c|
+        i = 1
+        while i <= k
+        c = c + 1
+            if c == 91
+                c = 97
+            end
+            if c == 123
+                c = 65
+            end
+        i = i + 1
+        end
+        array.push(c)
+    end
+end
+
+if n >= 1 && n <= 10000 && k >= 0 && k <= 10000
+    encrypt(s, k, array)
+end
+
+str = array.pack('c*')
+
+puts str
