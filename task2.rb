49f2426bf055591d58d44ffd02187d472b424b3a
anry88 <anry15@mail.ru>
2018-06-06 12:04:53 +0300

    task2 modified

--- task2.rb
+++ task2.rb
@@ -1,4 +1,4 @@
-s=ARGV[0]
+s=ARGV[0].dup
 k=ARGV[1].to_i
 n=s.size
 array = []
@@ -19,6 +19,8 @@ array = []
         array.push(c)
     end
 end
+
+s = s.force_encoding(Encoding::ASCII)
 
 if n >= 1 && n <= 10000 && k >= 0 && k <= 10000
     encrypt(s, k, array)
